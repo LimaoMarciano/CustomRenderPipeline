99b80279d5e089439c2a9391075ab012cdc8b154
Marcelo Silva <marceloapsilva@gmail.com>
2020-02-03 00:52:10 -0300

    Chapter 2 - Rendering 2.1 - Camera renderer

---
+++ Custom Render Pipeline/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer : MonoBehaviour
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    public void Render (ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+    }
+}
